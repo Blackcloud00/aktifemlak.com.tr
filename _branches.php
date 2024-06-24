241cc1f2a1466cf6078ae370b1bb7890ed05b4ca
Olgun Karabulut <olguntube@gmail.com>
2024-06-25 02:54:22 +0300

    first commit

---
+++ _branches.php
@@ -0,0 +1,47 @@
+<section class="branches normal">
+    <div class="container">
+        <h2 class="index_title bold">Şubelerimizi Keşfet</h2>
+        <div class="branch_list d-flex justify-content-between mt-5">
+            <div class="branch">
+                <img src="assets/image/icons/building.png" width="49" height="44" alt="Şube Adı">
+                <h6 class="bold">Şube Adı</h6>
+                <span class="s2">Acente Temsilcisi: Bora TÜRKOĞLU</span>
+                <span class="s2">Adres: Apt. 910 665 Lemke Loop, CO 06237-7979</span>
+                <span class="s2">Telefon: 0 850 850 85 85</span>
+                <a href="#" title="Şube Adı" class="branch_link"></a>
+            </div>
+            <div class="branch">
+                <img src="assets/image/icons/building.png" width="49" height="44" alt="Şube Adı">
+                <h6 class="bold">Şube Adı</h6>
+                <span class="s2">Acente Temsilcisi: Bora TÜRKOĞLU</span>
+                <span class="s2">Adres: Apt. 910 665 Lemke Loop, CO 06237-7979</span>
+                <span class="s2">Telefon: 0 850 850 85 85</span>
+                <a href="#" title="Şube Adı" class="branch_link"></a>
+            </div>
+            <div class="branch">
+                <img src="assets/image/icons/building.png" width="49" height="44" alt="Şube Adı">
+                <h6 class="bold">Şube Adı</h6>
+                <span class="s2">Acente Temsilcisi: Bora TÜRKOĞLU</span>
+                <span class="s2">Adres: Apt. 910 665 Lemke Loop, CO 06237-7979</span>
+                <span class="s2">Telefon: 0 850 850 85 85</span>
+                <a href="#" title="Şube Adı" class="branch_link"></a>
+            </div>
+            <div class="branch">
+                <img src="assets/image/icons/building.png" width="49" height="44" alt="Şube Adı">
+                <h6 class="bold">Şube Adı</h6>
+                <span class="s2">Acente Temsilcisi: Bora TÜRKOĞLU</span>
+                <span class="s2">Adres: Apt. 910 665 Lemke Loop, CO 06237-7979</span>
+                <span class="s2">Telefon: 0 850 850 85 85</span>
+                <a href="#" title="Şube Adı" class="branch_link"></a>
+            </div>
+            <div class="branch">
+                <img src="assets/image/icons/building.png" width="49" height="44" alt="Şube Adı">
+                <h6 class="bold">Şube Adı</h6>
+                <span class="s2">Acente Temsilcisi: Bora TÜRKOĞLU</span>
+                <span class="s2">Adres: Apt. 910 665 Lemke Loop, CO 06237-7979</span>
+                <span class="s2">Telefon: 0 850 850 85 85</span>
+                <a href="#" title="Şube Adı" class="branch_link"></a>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ _header.php
@@ -0,0 +1,46 @@
+<header class="normal">
+    <div class="container">
+        <div class="menu_header">
+            <div class="logo">
+                <img src="assets/image/logo.png" width="175" height="43" alt="Aktif Emlak Logo">
+            </div>
+            <ul class="menu_ul">
+                <li class="menu_li active">
+                    <a href="#" title="Ana Sayfa">Ana Sayfa</a>
+                </li>
+                <li class="menu_li">
+                    <a href="#" title="Satılık">Satılık</a>
+                </li>
+                <li class="menu_li">
+                    <a href="#" title="Kiralık">Kiralık</a>
+                </li>
+                <li class="menu_li">
+                    <a href="#" title="Konut">Konut</a>
+                </li>
+                <li class="menu_li">
+                    <a href="#" title="Arsa">Arsa</a>
+                </li>
+                <li class="menu_li">
+                    <a href="#" title="Villa">Villa</a>
+                </li>
+                <li class="menu_li">
+                    <a href="#" title="İşyeri">İşyeri</a>
+                </li>
+                <li class="menu_li">
+                    <a href="#" title="Kurumsal">Kurumsal</a>
+                </li>
+                <li class="menu_li">
+                    <a href="#" title="İletişim">İletişim</a>
+                </li>
+            </ul>
+            <ul class="menu_buttons">
+                <li>
+                    <a href="#" title="Giriş">Giriş</a>
+                </li>
+                <li>
+                    <a href="#" title="Kayıt Ol" class="active_kayit">Kayıt Ol</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
